666e3d9f7a90a4e7665c9d455dd6a9495b0622c8
Miguel Cardenas <miguelecsx@gmail.com>
2024-02-16 10:09:11 -0500

    feat: improve naming conventions

---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+    Object Oriented Programming
+    - University of Medellin - UdeM
+    - Students: [Emanuel Cabrera, Miguel Cardenas]
+    - Date: 2024-02-14
+"""
+
+main():
+    ...
+
+if __name__ = "__main__":
+    main()
