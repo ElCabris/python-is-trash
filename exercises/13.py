f5aa0af1fb4907106bd87ae52fec081a7aa18d53
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-02-15 20:01:05 -0500

    Exercise 13

---
+++ exercises/13.py
@@ -0,0 +1,9 @@
+# Crear un programa que pida al usuario ingresar dos números y calcule su suma, resta, multiplicación y división
+
+a = float(input("ingresa el primer número: "))
+b = float(input("ingresa el segundo número: "))
+
+print(f"Suma: {a + b}")
+print(f"Resta: {a - b}")
+print(f"multiplicación {a * b}")
+print(f"división {a / b}")
